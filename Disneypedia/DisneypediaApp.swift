7a2a2d858c58f6036549c15ef5a48aa03439bc8d
BryllieBrinkley <jibryllbrink@yahoo.com>
2024-10-16 11:57:39 -0400

    Initial Commit

---
+++ Disneypedia/DisneypediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneypediaApp.swift
+//  Disneypedia
+//
+//  Created by Jibryll Brinkley on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct DisneypediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
